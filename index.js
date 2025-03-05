f47d987be0b0098d144a050e2f79d5195813e1b7
Erykah <erkahwanga@gmail.com>
2025-03-05 11:21:06 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+// Declare companyName with const as 'Scuber'
+const companyName = 'Scuber';
+
+// Declare mostProfitableNeighborhood with let as it may change
+let mostProfitableNeighborhood = 'Chelsea';
+
+
+// Declare companyCeo with const as it shouldn't change
+let companyCeo = 'Susan Smith';
+
